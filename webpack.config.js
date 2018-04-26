ec302cbb32ff629007f22b86fa5aab12b4899707
xunlu <xunlu@helijia.com>
2018-04-26 16:46:31 +0800

    调试webpack最新版

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,31 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-stage-0'],
+            plugins: [
+              ['@babel/plugin-transform-runtime', {
+                helpers: false,
+                polyfill: false,
+                regenerator: true,
+                moduleName: '@babel/runtime'
+              }]
+            ],
+            babelrc: false,
+            cacheDirectory: true
+          }
+        },
+        include: [
+          path.join(__dirname, '../node_modules/@hlj')
+        ]
+      }
+    ]
+  }
 };
